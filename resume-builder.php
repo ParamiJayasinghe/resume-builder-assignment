069312bcea51415ed1d3a86c6c140f83a0b971ce
ParamiJayasinghe <apurvajayasinghe@gmail.com>
2026-09-19 18:34:00 +0530

    init: initialize plugin structure,git and npm build tools

---
+++ resume-builder.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Resume Builder
+ * Description: A React-powered resume builder with custom REST API endpoints.
+ * Version: 1.0.0
+ * Author: Parami Jayasinghe
+ */
+
+if( ! defined( 'ABSPATH' )){
+    exit;
+}
+
+define ( 'RESUME_BUILDER_PATH', plugin_dir_path( __FILE__));
+define ( 'RESUME_BUILDER_URL', plugin_dir_url( __FILE__));
